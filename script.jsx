716f4c08fd4f8f461444e7c93c533cb03e34b8d1
kinskin <md.asshikin@gmail.com>
2019-08-29 18:39:32 +0800

    done the second last further

--- script.jsx
+++ script.jsx
@@ -5,7 +5,8 @@ class List extends React.Component {
     this.state = {
       word:"",
       validation: '',
-      list : []
+      list : [],
+      doneList: []
 
     }
   }
@@ -32,6 +33,36 @@ class List extends React.Component {
     }
   }
 
+  deleteTodo(){
+    console.log("inside delete Item")
+    let deleteItem = event.target.value
+    let doneList = this.state.doneList
+
+    let deleteItemIndex = doneList.findIndex(item => item === deleteItem)
+    doneList.splice(deleteItemIndex, 1)
+    this.setState({doneList: doneList})
+  }
+
+  doneTodo(){
+    let toDoItem = event.target.value
+    let list = this.state.list
+    let doneList = this.state.doneList
+
+
+    let itemIndex = list.findIndex(item => item === toDoItem )
+    console.log("deleting", itemIndex);
+
+
+    let newDoneList = list.splice(itemIndex, 1)
+
+
+    doneList.push(newDoneList[0])
+    console.log("updated", list)
+
+    this.setState({list: list, doneList: doneList})
+
+  }
+
   changeHandler(){
     // console.log(event.target.value){
         // if (event.target.value === true){
@@ -50,23 +81,60 @@ class List extends React.Component {
       // render the list with a map() here
       let list = this.state.list.map((list)=>{
         return(
-            <p>[ ] {list}</p>
+            <div>
+                <div className='row'>
+                    <div className='col-6'>
+                        <p>{list}</p>
+                    </div>
+                    <div className='col-6 text-right'>
+                        <button className = 'btn btn-sm btn-outline-success'onClick={(event)=>{this.doneTodo(event)}} value={list} >Completed</button>
+                    </div>
+                </div>
+            </div>
+            )
+      })
+      let doneList = this.state.doneList.map((doneList)=>{
+        return(
+            <div>
+                <div className='row'>
+                    <div className='col-6'>
+                        <p>{doneList}</p>
+                    </div>
+                    <div className='col-6 text-right'>
+                        <button className='btn btn-sm btn-outline-danger'onClick={(event)=>{this.deleteTodo(event)}} value={doneList} >Delete Item</button>
+                    </div>
+                </div>
+            </div>
             )
       })
       console.log("rendering");
       return (
         <div className='container'>
-            <div className="">
-                <h2>Todo List</h2>
-                <p>{this.state.validation}</p>
+            <div className= "header">
+                <div className="card-header text-center">
+                    <h2>Todo List</h2>
+                    <p>{this.state.validation}</p>
+                </div>
             </div>
-            <div className="list">
-              <input onChange={(event)=>{this.changeHandler(event)}} onKeyDown={(event)=>{this.changeHandler(event)}} value={this.state.word}/>
-              <button onClick={(event)=>{this.addItem(event)}} onKeyDown={(event)=>{this.addItem(event)}} value={this.state.word} >add item</button>
+            <div className="header form-inline">
+                <div className="col-8 offset-2 text-center">
+                  <input className='form-control' onChange={(event)=>{this.changeHandler(event)}} onKeyDown={(event)=>{this.changeHandler(event)}} value={this.state.word} placeholder={'What is your todo list?'}/>
+                  <button className= 'btn btn-outline-primary'onClick={(event)=>{this.addItem(event)}} onKeyDown={(event)=>{this.addItem(event)}} value={this.state.word} >add item</button>
+                </div>
             </div>
-            <div>
-                <h6>Today list</h6>
-                {list}
+            <div className='row'>
+                <div className='col-6'>
+                    <h6 className='card-header'>Pending</h6>
+                    <div className='card-body'>
+                        {list}
+                    </div>
+                </div>
+                <div className='col-6'>
+                    <h6 className='card-header'>Completed</h6>
+                    <div className='card-body'>
+                        {doneList}
+                    </div>
+                </div>
             </div>
         </div>
       );
