27b3ab40d699cf0f2bd7b4cb56b516cef1a20bcf
kinskin <md.asshikin@gmail.com>
2019-08-29 15:20:32 +0800

    further no. 3

--- script.jsx
+++ script.jsx
@@ -4,26 +4,70 @@ class List extends React.Component {
 
     this.state = {
       word:"",
+      validation: '',
       list : []
+
     }
   }
 
   addItem(){
-    debugger;
+    console.log(this.state.word.length)
+    let validation = this.state.validation
+    if(this.state.word.length > 1 && this.state.word.length <200){
+        validation = ''
+        let list = this.state.list
+        let word = this.state.word
+        word = ''
+        list.push(event.target.value)
+        this.setState({list: list, word: word, validation: validation})
+        console.log(this.state.list);
+    }
+    else if(this.state.word.length <= 1){
+        validation = "To do list is too short"
+        this.setState({validation: validation})
+    }
+    else if(this.state.word.length >= 200){
+        validation = "To do list is too long"
+        this.setState({validation: validation})
+    }
   }
 
   changeHandler(){
-    debugger;
+    // console.log(event.target.value){
+        // if (event.target.value === true){
+            let word = event.target.value
+            if(event.keyCode === 13){
+                this.setState({word: word})
+                this.addItem()
+            }
+            else{
+                this.setState({word: word})
+            // }
+        }
   }
 
   render() {
       // render the list with a map() here
-
+      let list = this.state.list.map((list)=>{
+        return(
+            <p>[ ] {list}</p>
+            )
+      })
       console.log("rendering");
       return (
-        <div className="list">
-          <input onChange={()=>{this.changeHandler()}} value={this.state.word}/>
-          <button onClick={()=>{this.addItem()}}>add item</button>
+        <div className='container'>
+            <div className="">
+                <h2>Todo List</h2>
+                <p>{this.state.validation}</p>
+            </div>
+            <div className="list">
+              <input onChange={(event)=>{this.changeHandler(event)}} onKeyDown={(event)=>{this.changeHandler(event)}} value={this.state.word}/>
+              <button onClick={(event)=>{this.addItem(event)}} onKeyDown={(event)=>{this.addItem(event)}} value={this.state.word} >add item</button>
+            </div>
+            <div>
+                <h6>Today list</h6>
+                {list}
+            </div>
         </div>
       );
   }
@@ -33,4 +77,3 @@ ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
